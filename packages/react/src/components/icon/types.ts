c3f9ae9eaa23a23b6d118895c2f63d6f269a464b
Pablo Coronel <pablo.coronel@garajedeideas.com.ar>
2024-01-03 17:11:47 -0300

    rename icon size tokens in React package

--- packages/react/src/components/icon/types.ts
+++ packages/react/src/components/icon/types.ts
@@ -1,5 +1,5 @@
 
-export type IconSizeType = "sm" | "md" | "lg" | "xl";
+export type IconSizeType = "small" | "medium" | "large" | "extra-large";
 export type IconPropTypes = {
   name?: string;
   size?: IconSizeType;
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,17 +1,8 @@
-import { Button } from './components/button'
-import { Icon } from './components/icon'
-import { Image } from './components/image'
-import { Switch } from './components/switch'
-import { ThemeWrapper } from './components/themeWrapper'
-import { Text } from './components/text'
-import { TextArea } from './components/textarea'
+import { Button } from "./components/button";
+import { Icon } from "./components/icon";
+import { Image } from "./components/image";
+import { Switch } from "./components/switch";
+import { ThemeWrapper } from "./components/themeWrapper";
+import { TextArea } from "./components/textarea";
 
-export { 
-    Button, 
-    Icon, 
-    Image,
-    Switch, 
-    ThemeWrapper, 
-    Text, 
-    TextArea,
-}
+export { Button, Icon, Image, Switch, ThemeWrapper, TextArea };
